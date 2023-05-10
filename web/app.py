99e0a2df85198bb397c59de223b21b06f11a8aea
raysinm <raysin.maya1@gmail.com>
2023-05-11 00:55:36 +0300

    Changed file structure to support grpc

--- web/app.py
+++ web/app.py
@@ -29,41 +29,7 @@ def response_matrix_list():
 
 @app.route('/update_config', methods=['POST'])
 def update_config():
-    parameters = {}
-    
-    with open(config_path, 'r') as f:
-        config_data = json.load(f)
-
-    form_data = {
-    'tip_cell': {
-    "p_migrate": request.form.get('tip_cell_p_migrate')
-    },
-    'attractor_cell' : {
-        "attraction_generated": request.form.get('attractor_cell_attraction_generated')
-    },
-    'stalk_cell' : {
-        "p_sprout": request.form.get('stalk_cell_p_sprout')
-    },
-    'attraction' : {
-        "decay_coef": request.form.get('attraction_decay_coef'),
-        "update_precision": request.form.get('attraction_update_precision')
-    },
-    'graphics': {
-        'generations' : request.form.get('graphics_generations')
-    } 
-    }
-    for form_key, form_value in form_data.items():
-        for param, numerical_value in form_value.items():
-            if form_key in config_data['defaults'].keys() and numerical_value != None and numerical_value != '':
-                config_data['defaults'][form_key][param] = float(numerical_value)
-    
-    form_data_generations = form_data['graphics']['generations'] #TODO: fix me
-    if form_data_generations != None and form_data_generations != '':
-        config_data['graphics']['generations'] = int(form_data_generations)    
-    # Write the updated data back to the file
-    with open(config_path, 'w') as f:
-        json.dump(config_data, f, indent=4)
-
+    # Should call the server with the form dict from the html  
     home = app.url_for(endpoint='index')
     return app.redirect(home)
 
